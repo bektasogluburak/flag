a69b9914a4da41e9919e2e32a9ede75cc96926e4
burak <burakbektasoglu12@gmail.com>
2023-12-24 14:25:39 +0300

    form resize disable edildi.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -91,6 +91,9 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Flag Game";
